7209c2aae21dfbcc835f0852e7ab91ddac1e63e4
Stelfy <bastianini.alex@gmail.com>
2023-03-29 20:32:27 +0200

    added debounce to +- buttons in workout

--- src/ExerciseList.js
+++ src/ExerciseList.js
@@ -1,21 +1,25 @@
 import SetList from "./SetList";
 
 const ExerciseList = ({ exercises, workoutID }) => {
-    
-    return (
-        <div className="exercise">
-            {exercises.map( (ex, index) => {
-                return (
-                    <div key={index}>
-                        <h3>{ ex.name }</h3>
-                        <div className="set">
-                            <SetList exercises = { exercises } sets={ ex } exerciseIndex={ index } workoutID={ workoutID } />
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
-    );
-}
- 
+  return (
+    <div className="exercise">
+      {exercises.map((ex, index) => {
+        return (
+          <div key={index}>
+            <h3>{ex.name}</h3>
+            <div className="set">
+              <SetList
+                exercises={exercises}
+                exercise={ex}
+                exerciseIndex={index}
+                workoutID={workoutID}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 export default ExerciseList;
--- src/SetList.js
+++ src/SetList.js
@@ -1,79 +1,112 @@
-import { useState } from 'react';
-import { doc, updateDoc } from '@firebase/firestore';
-import { db } from './firebase';
+import { useEffect, useRef, useState } from "react";
+import { doc, updateDoc } from "@firebase/firestore";
+import { db } from "./firebase";
 
-const SetList = ({ exercises, sets, exerciseIndex, workoutID }) => {
+const SetList = ({ exercises, exercise, exerciseIndex, workoutID }) => {
+  const [error, setError] = useState("");
+  const [repCounts, setRepCounts] = useState(exercise.reps);
+  const [weights, setWeights] = useState(exercise.weights);
+  const isMounted = useRef(false);
 
+  const dataPatch = (data) => {
+    console.log("patching..");
+    //changes the specified field of a document without overwriting it all (similar to 'PATCH')
+    updateDoc(doc(db, "workouts", workoutID), {
+      exercises: data,
+    })
+      .then(() => {
+        setRepCounts(data[exerciseIndex].reps);
+        setWeights(data[exerciseIndex].weights);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
 
-    const [error, setError] = useState('');
-    const [exSets, setExSets] = useState(sets.reps);
+  const exSetPatch = () => {
+    const newExercises = JSON.parse(JSON.stringify(exercises));
+    newExercises[exerciseIndex].reps = newRepCounts;
+    newExercises[exerciseIndex].weights = newWeights;
+    dataPatch(newExercises);
+  };
 
-    const dataPatch = (data) => {
+  let newRepCounts = repCounts;
+  let newWeights = weights;
 
-        //changes the specified field of a document without overwriting it all (similar to 'PATCH')
-        updateDoc(doc(db, "workouts", workoutID), {
-            exercises: data,
-        })
-        .then(() => {
-            setExSets([...data[exerciseIndex].reps]);   //updates the array of reps for each exercise
-        })
-        .catch((err) => {
-            setError(err.message);
-        })
+  useEffect(() => {
+    if (isMounted.current) {
+      const setChange = setTimeout(() => {
+        exSetPatch();
+      }, 1000);
 
+      return () => clearTimeout(setChange);
+    } else {
+      isMounted.current = true;
     }
+  }, [repCounts, weights]);
 
-    const handleAddRep = (repIndex) => {
-        const newExercises = [...exercises];
-        const updatedRep = (parseInt(newExercises[exerciseIndex].reps[repIndex])+1).toString();
-        newExercises[exerciseIndex].reps[repIndex] = updatedRep;
-        dataPatch(newExercises);
-    }
+  const handleModifyRepCount = (repIndex, diff) => {
+    newRepCounts = repCounts.map((rep, index) => {
+      if (index === repIndex) return (parseFloat(rep) + diff).toString();
+      return rep;
+    });
+    setRepCounts(newRepCounts);
+  };
 
-    const handleRemoveRep = (repIndex) => {
-        const newExercises = [...exercises];
-        const updatedRep = (parseInt(newExercises[exerciseIndex].reps[repIndex])-1).toString();
-        newExercises[exerciseIndex].reps[repIndex] = updatedRep;
-        dataPatch(newExercises);
-    }
+  const handleModifyWeight = (weightIndex, diff) => {
+    newWeights = weights.map((weight, index) => {
+      if (index === weightIndex) return (parseFloat(weight) + diff).toString();
+      return weight;
+    });
+    setWeights(newWeights);
+  };
 
-    const handleAddWeight = (weightIndex) => {
-        const newExercises = [...exercises];
-        const updatedRep = (parseFloat(newExercises[exerciseIndex].weights[weightIndex]) + 1.25 ).toString();
-        newExercises[exerciseIndex].weights[weightIndex] = updatedRep;
-        dataPatch(newExercises);
-    }
+  return (
+    <div className="sets">
+      {error && <div className="error">{error}</div>}
+      {repCounts.map((rep, index) => {
+        return (
+          <div className="set" key={index}>
+            <div className="rep-buttons">
+              <button
+                type="button"
+                id="addRep"
+                onClick={() => handleModifyRepCount(index, 1)}
+              >
+                +
+              </button>
+              <button
+                type="button"
+                id="removeRep"
+                onClick={() => handleModifyRepCount(index, -1)}
+              >
+                -
+              </button>
+            </div>
+            <div className="set-details">
+              {rep} X {weights[index]} Kg
+            </div>
+            <div className="weight-buttons">
+              <button
+                type="button"
+                id="addWeight"
+                onClick={() => handleModifyWeight(index, 1.25)}
+              >
+                +
+              </button>
+              <button
+                type="button"
+                id="removeWeight"
+                onClick={() => handleModifyWeight(index, -1.25)}
+              >
+                -
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-    const handleRemoveWeight = (weightIndex) => {
-        const newExercises = [...exercises];
-        const updatedRep = (parseFloat(newExercises[exerciseIndex].weights[weightIndex]) - 1.25 ).toString();
-        newExercises[exerciseIndex].weights[weightIndex] = updatedRep;
-        dataPatch(newExercises);
-    }
-
-
-    return (
-        <div className="sets">
-            { error && <div className="error">{ error }</div>}
-            {exSets.map((rep, index) => {
-                return (
-                    <div className="set" key={ index }>
-                        <div className="rep-buttons">
-                            <button type='button' id="addRep" onClick={() => handleAddRep(index)}>+</button>
-                            <button type='button' id="removeRep" onClick={() => handleRemoveRep(index)}>-</button> 
-                        </div>
-                        <div className="set-details">
-                            { rep }  X  { sets.weights[index] } Kg
-                        </div>  
-                        <div className="weight-buttons">
-                            <button type='button' id="addWeight" onClick={() => handleAddWeight(index)}>+</button>
-                            <button type='button' id="removeWeight" onClick={() => handleRemoveWeight(index)}>-</button>
-                        </div>
-                    </div>
-                );       
-            })}
-        </div>
-    );
-}
- 
 export default SetList;
